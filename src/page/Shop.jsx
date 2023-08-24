7cb0bc7dafce328384806990e1cb73d118ea6c24
mahbub alam <ma01776879@gmail.com>
2023-08-24 11:43:02 +0600

    card

--- src/page/Shop.jsx
+++ src/page/Shop.jsx
@@ -16,6 +16,10 @@ import StarIcon from "@mui/icons-material/Star";
 import { notifySuccess } from "../components/common/notifySuccess";
 
 export const Shop = () => {
+  const [Products, setProducts] = useState(null);
+  console.log(Products,'Products888888888')
+  const [show, setShow] = useState(false);
+  const [prod, setProd] = useState(null);
   const [rating, setRating] = useState(0); // initial rating value
   const [items, setItems] = useState(false); // initial rating value
 
@@ -25,11 +29,7 @@ export const Shop = () => {
     // other logic
   };
 
-  const domain = "https://availtrade.com/public/images/";
-  const [Products, setProducts] = useState(null);
-
-  const [show, setShow] = useState(false);
-  const [prod, setProd] = useState(null);
+  // const domain = "https://availtrade.com/public/images/";
 
   const handleClose = () => setShow(false);
   const handleModal = () => setShow(true);
@@ -38,17 +38,12 @@ export const Shop = () => {
     handleModal();
   };
 
-  // const notify = () =>   toast.success("product add to cart!", {
-  //   position: toast.POSITION.TOP_CENTER
-  // });
-
   //redux get product
   const dispatch = useDispatch();
   const product = useSelector((state) => state.product);
 
   //add product
   const addToCart = (product) => {
-    console.log(product, "=====");
     dispatch(incrementCounter(product));
     notifySuccess();
   };
@@ -77,20 +72,74 @@ export const Shop = () => {
   }, []);
 
   return (
-    <div style={{ background: "#f2eeed" }}>
+    <div className="bg-white mt-[20px]">
       <div className="container">
-        <div className="section-title text-center">
-          <h4>Latest Discounted Products</h4>
-          <hr />
-          <p className="text-muted">
-            See Our latest discounted products below. Choose your daily needs{" "}
-            <br />
-            from here and get a special discount with free shipping.
-          </p>
+        <div className="mt-4 flex items-center justify-between">
+          <span className="border-b w-1/5 lg:w-2/5"></span>
+          <a
+            href="#"
+            className="text-2xl font-medium  text-center text-gray-500 uppercase "
+          >
+            Discount Product
+          </a>
+          <span className="border-b w-1/5 lg:w-2/5"></span>
         </div>
       </div>
 
-      <div>
+      <section
+        id="Projects"
+        className="w-fit mt-[20px] mx-auto grid grid-cols-1 lg:grid-cols-4 md:grid-cols-3 justify-items-center justify-center gap-y-20 gap-x-14  mb-5"
+      >
+        {Products?.map((item, i) => (
+          <div className="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
+            <a href="#">
+              <img
+                src={item?.image}
+                alt="Product"
+                className="h-80 w-72 object-fill rounded-t-xl"
+                onClick={() => handleShow(item)}
+                
+              />
+              <div className="px-4 py-3 w-72">
+                <span className="text-gray-400 mr-3 uppercase text-xs">
+                  Brand
+                </span>
+                <p className="text-lg font-bold text-black truncate block capitalize">
+                {item?.category}
+                </p>
+                <div className="flex items-center">
+                  <p className="text-lg font-semibold text-black cursor-auto my-3">
+                    {item?.price}
+                  </p>
+                  <del>
+                    <p className="text-sm text-gray-600 cursor-auto ml-2">
+                      $199
+                    </p>
+                  </del>
+                  <div className="ml-auto">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="20"
+                      height="20"
+                      fill="currentColor"
+                      className="bi bi-bag-plus"
+                      viewBox="0 0 16 16"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M8 7.5a.5.5 0 0 1 .5.5v1.5H10a.5.5 0 0 1 0 1H8.5V12a.5.5 0 0 1-1 0v-1.5H6a.5.5 0 0 1 0-1h1.5V8a.5.5 0 0 1 .5-.5z"
+                      />
+                      <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z" />
+                    </svg>
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
+        ))}
+      </section>
+
+      {/* <div>
         {Products?.map((item, i) => (
           <div className="row" style={{ display: "inline-block" }} key={i}>
             <div className="col-md-3">
@@ -137,7 +186,7 @@ export const Shop = () => {
                         <FavoriteIcon />
                       </span>{" "}
                     </p>
-                    {/* <p style={{fontSize:'10px'}}><span style={{color:'goldenrod'}}><StarIcon/><StarIcon/><StarIcon/><StarIcon/><StarIcon/></span></p> */}
+                    <p style={{fontSize:'10px'}}><span style={{color:'goldenrod'}}><StarIcon/><StarIcon/><StarIcon/><StarIcon/><StarIcon/></span></p>
 
                     <CardActions style={{ justifyContent: "center" }}>
                       <Button
@@ -148,19 +197,17 @@ export const Shop = () => {
                       >
                         AddToCart
                       </Button>
-                      {/* <Card  className='addCard'  id='cardid'>
-<p onClick={()=>setItems(!items)}><span><svg style={{color:'tomato',height:'25px',width:'40px'}} stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 512 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M460 160h-88v-12A116.13 116.13 0 00258.89 32h-5.78A116.13 116.13 0 00140 148v12H52a4 4 0 00-4 4v300a16 16 0 0016 16h384a16 16 0 0016-16V164a4 4 0 00-4-4zm-280-11c0-41.84 33.41-76.56 75.25-77A76.08 76.08 0 01332 148v12H180zm156 187h-64v64h-32v-64h-64v-32h64v-64h32v64h64z"></path></svg></span></p>
+                      <Card  className='addCard'  id='cardid'>
+                        <p onClick={()=>setItems(!items)}><span><svg style={{color:'tomato',height:'25px',width:'40px'}} stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 512 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M460 160h-88v-12A116.13 116.13 0 00258.89 32h-5.78A116.13 116.13 0 00140 148v12H52a4 4 0 00-4 4v300a16 16 0 0016 16h384a16 16 0 0016-16V164a4 4 0 00-4-4zm-280-11c0-41.84 33.41-76.56 75.25-77A76.08 76.08 0 01332 148v12H180zm156 187h-64v64h-32v-64h-64v-32h64v-64h32v64h64z"></path></svg></span></p>
+                      </Card>
 
-</Card> */}
-
-                      {/* { items &&
-            
-            <div className='input-group' style={{width:'100px'}} id='divShow' >
-            <button type='button' className='input-group-text' style={{width:'15px'}} onClick={()=>''}>-</button>
-            <div className='form-control text-center' style={{width:'15px'}}>{prod?.start_quantity}</div>
-            <button type='button' className='input-group-text' style={{width:'15px'}} onClick={()=>addToCart(prod)}>+</button>
-           </div>
-            } */}
+                      { items &&
+                          <div className='input-group' style={{width:'100px'}} id='divShow' >
+                          <button type='button' className='input-group-text' style={{width:'15px'}} onClick={()=>''}>-</button>
+                          <div className='form-control text-center' style={{width:'15px'}}>{prod?.start_quantity}</div>
+                          <button type='button' className='input-group-text' style={{width:'15px'}} onClick={()=>addToCart(prod)}>+</button>
+                        </div>
+                      }
                     </CardActions>
                   </Card.Text>
                 </Card.Body>
@@ -168,7 +215,7 @@ export const Shop = () => {
             </div>
           </div>
         ))}
-      </div>
+      </div> */}
 
       <div>
         <Modal show={show} onHide={handleClose}>
