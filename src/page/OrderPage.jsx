16b7239d0dc566e5c9de3fcf6b4f0cd8d6a822e5
MAHBUB0177 <ma01776879@gmail.com>
2023-08-26 20:56:46 +0600

    update

--- src/page/OrderPage.jsx
+++ src/page/OrderPage.jsx
@@ -2,16 +2,16 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { domain, getheader } from '../env';
 import Cookies from 'js-cookie'
-import {  Box } from '@material-ui/core';
+import { Box } from '@material-ui/core';
 import { Link } from 'react-router-dom';
-import { Button  } from '@mui/material';
+import { Button } from '@mui/material';
 import { Typography } from '@mui/material';
 // import Swal from 'sweetalert2'
 import DeleteIcon from '@mui/icons-material/Delete';
-import {Card} from 'react-bootstrap';
-import {  IconButton } from '@mui/material';
-import { useSelector ,useDispatch} from 'react-redux';
-import {decrementCounter} from '../Service/Action/Action'
+import { Card } from 'react-bootstrap';
+import { IconButton } from '@mui/material';
+import { useSelector, useDispatch } from 'react-redux';
+import { decrementCounter } from '../Service/Action/Action'
 import CurrencyRupeeIcon from '@mui/icons-material/CurrencyRupee';
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
@@ -19,127 +19,202 @@ import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-import {   Container, Nav, Table } from 'react-bootstrap';
+import { Container, Nav, Table } from 'react-bootstrap';
 import { TabTitle } from '../utils/FunctionTitle';
 import { notifyError } from '../components/common/notifySuccess';
 const OrderPage = () => {
   TabTitle('Amar Bazar | OrderPage')
   const domain = "https://availtrade.com/public/images/";
-  const dispatch=useDispatch()
+  const dispatch = useDispatch()
 
 
   // total cout":
-  const cartItem=useSelector(state=>state.product)
-  let total=0;
-  for (let i=0;i<cartItem.length;i++){
-    let prod=cartItem[i]
-    total=total+prod.price * prod.qnty
+  const cartItem = useSelector(state => state.product)
+  let total = 0;
+  for (let i = 0; i < cartItem.length; i++) {
+    let prod = cartItem[i]
+    total = total + prod.price * prod.qnty
   }
   // for (let i=0;i<cartItem.length;i++){
   //   let prod=cartItem[i];
   //   total=total+Number(prod.product_price);
   // }
-  var tax=Number(total/10);
+  var tax = Number(total / 10);
 
-//remove cart:
-const removeItem=(product)=>{
-  dispatch(decrementCounter(product))
-  notifyError()
+  //remove cart:
+  const removeItem = (product) => {
+    dispatch(decrementCounter(product))
+    notifyError()
 
-}
+  }
 
   return (
-    <div className='  container col-md-12' style={{  marginTop: '30px',marginLeft:'20px',marginRight:'20px' }} >
-      <div class="col-12 col-lg-8 text-center">
-          {/* <span class="dri dri-cart fa-2x bg-primary rounded-circle"></span> */}
-          { cartItem?.length >0 &&
-          <h2 class="mt-3" style={{marginLeft:'50px'}}>Shopping <font class="text-primary">Cart</font></h2>
+    // <div className='  container col-md-12' style={{ marginTop: '30px', marginLeft: '20px', marginRight: '20px' }} >
+    //   <div class="col-12 col-lg-8 text-center">
+    //     {cartItem?.length > 0 &&
+    //       <h2 class="mt-3" style={{ marginLeft: '50px' }}>Shopping <font class="text-primary">Cart</font></h2>
+    //     }
+    //   </div>
 
 
-          }
-      </div>
+    //   <div className="row d-flex" >
+    //     <div className='col-md-7'>
+    //       <ToastContainer />
+    //       {cartItem?.length > 0 &&
+    //         <div className='' >
+    //           <Table>
+    //             <thead>
+    //               <tr>
+    //                 <th>product</th>
+    //                 <th>name</th>
+    //                 <th>Quantity</th>
+    //                 <th>total price</th>
+    //                 <th>Action</th>
+    //               </tr>
+    //             </thead>
 
+    //             <tbody>
+
+    //               {
+    //                 cartItem?.map(item =>
+    //                   <tr>
+    //                     <td><img src={item?.image} alt="" style={{ height: '80px', width: '70px', clipPath: 'circle()' }}></img></td>
+    //                     <td>{item?.category.substring(0, 6)}</td>
+    //                     <td>{item?.qnty}</td>
+    //                     <td>{item?.price * item?.qnty}TK</td>
+    //                     <td><IconButton ><span style={{ color: 'red' }} onClick={() => removeItem(item)}><DeleteIcon /></span></IconButton></td>
+
+    //                   </tr>
+    //                 )
+    //               }
+
+    //             </tbody>
+    //           </Table>
+
+    //           <div style={{ display: 'flex' }}>
+    //             <div className='col-6 ' >
+    //               <Link to='/' style={{ textDecoration: 'none' }}>
+    //                 <Button variant="contained" color='secondary' style={{ width: '160px', }} ><ArrowBackIcon />Add Cart</Button>
+    //               </Link>
+    //             </div>
+
+    //             <Link to='/checkout' style={{ textDecoration: 'none' }}>
+    //               <div className='col-6 mb-5'>
+    //                 <Button variant="contained" color="info" style={{ width: '160px', marginLeft: '5px' }} >Checkout<ArrowForwardIcon /></Button>
+
+    //               </div>
+    //             </Link>
+
+    //           </div>
+    //         </div>
+    //       }
+    //     </div>
+
+    //     {
+    //       cartItem?.length <= 0 && <div style={{ height: '200px' }}>
+    //         <h2 style={{ textAlign: 'center', color: 'red', marginTop: '40px' }}> Yor Cart Is Empty !!!!!</h2>
+
+    //       </div>
+
+    //     }
+
+
+    //     {cartItem?.length > 0 &&
+    //       <div className='col-md-5 ' style={{ background: '#dce0dd', height: '300px', marginTop: '40px', marginBottom: '5px' }}>
+
+    //         <h4 style={{ marginLeft: '0px', marginTop: '10px', color: 'tomato' }}>Order Summary</h4>
+    //         <hr />
+    //         <h6 style={{ marginTop: '30px', marginRight: '390px' }}>Subtotal:<span style={{ marginLeft: '200px' }}>{total.toFixed(2)}Tk</span></h6>
+    //         <h6 style={{ marginRight: '390px' }}>Charge:<span style={{ marginLeft: '200px' }}>{total.toFixed(2)}Tk</span></h6>
+    //         <h6 style={{ marginRight: '390px' }}>Discount:<span style={{ marginLeft: '200px' }}>0.00Tk</span></h6>
+    //         <br />
+    //         <hr />
+    //         <h6 style={{ marginRight: '390px', color: 'tomato' }}>Total:<span style={{ marginLeft: '220px', color: 'tomato' }}>{total.toFixed(2)}Tk</span></h6>
+
+    //       </div>
+    //     }
+
+    //   </div>
+    // </div>
+
+    <>
     
-<div className="row d-flex" >
-<div className='col-md-7'>
-<ToastContainer/>
-        { cartItem?.length > 0 && 
-              <div className='' >
-                    <Table>
-                            <thead>
-                              <tr>
-                                <th>product</th>
-                                <th>name</th>
-                                <th>Quantity</th>
-                                <th>total price</th>
-                                <th>Action</th>
-                              </tr>
-                            </thead>
-
-                            <tbody>
-                              
-                            {
-                                cartItem?.map(item =>
-                                  <tr>
-                                    <td><img src={item?.image} alt="" style={{  height: '80px', width: '70px' ,clipPath:'circle()'}}></img></td>
-                                    <td>{item?.category.substring(0,6)}</td>
-                                    <td>{item?.qnty}</td>
-                                    <td>{item?.price * item?.qnty}TK</td>
-                                    <td><IconButton ><span style={{color:'red'}} onClick={()=>removeItem(item)}><DeleteIcon/></span></IconButton></td> 
-
-                                  </tr>
-                                  )
-                              }
-                             
-                            </tbody>
-                       </Table>
-                       
-                       <div style={{ display: 'flex' }}>
-                          <div className='col-6 ' >
-                              <Link to='/' style={{ textDecoration: 'none' }}>
-                                    <Button variant="contained" color='secondary' style={{ width: '160px',  }} ><ArrowBackIcon/>Add Cart</Button>
-                              </Link>
-                          </div>
-
-                        <Link to='/checkout' style={{textDecoration:'none'}}>
-                        <div className='col-6 mb-5'>
-                              <Button variant="contained" color="info" style={{ width: '160px' ,marginLeft:'5px'}} >Checkout<ArrowForwardIcon/></Button>
-
-                          </div>
-                        </Link>
-  
-                  </div>
-                  </div>
-          }
- </div>
-
-          {    
-          // className='alert alert-info '
-                        cartItem?.length <=0 && <div style={{height:'200px'}}>
-                         <h2 style={{textAlign:'center',color:'red',marginTop:'40px'}}> Yor Cart Is Empty !!!!!</h2>
-                         
-                        </div>
-                        
-          }
+    <div class="bg-gray-100  py-8">
+    <div class="container mx-auto px-4">
+    {cartItem?.length > 0 &&  <h1 class="text-2xl font-semibold mb-4">Shopping Cart</h1>}
+        <div class="flex flex-col md:flex-row gap-4">
+        <ToastContainer />
+           
 
 
-  {  cartItem?.length > 0 && 
-          <div className='col-md-5 ' style={{background:'#dce0dd',height:'300px',marginTop:'40px',marginBottom:'5px'}}>
-            
-                <h4 style={{marginLeft:'0px',marginTop:'10px',color:'tomato'}}>Order Summary</h4>
-                <hr/>
-                <h6 style={{marginTop:'30px',marginRight:'390px'}}>Subtotal:<span style={{marginLeft:'200px'}}>{total.toFixed(2)}Tk</span></h6>
-                <h6 style={{marginRight:'390px'}}>Charge:<span style={{marginLeft:'200px'}}>{total.toFixed(2)}Tk</span></h6>
-                <h6 style={{marginRight:'390px'}}>Discount:<span style={{marginLeft:'200px'}}>0.00Tk</span></h6>
-                <br/>
-                <hr/>
-                <h6 style={{marginRight:'390px',color:'tomato'}}>Total:<span style={{marginLeft:'220px',color:'tomato'}}>{total.toFixed(2)}Tk</span></h6>
-                
+            {
+              cartItem?.length > 0 &&  <div class="md:w-3/4">
+              <div class="bg-white rounded-lg shadow-md p-6 mb-4">
+                  <table class="w-full">
+                      <thead>
+                          <tr>
+                              <th class="text-left font-semibold">Product</th>
+                              <th class="text-left font-semibold">Price</th>
+                              <th class="text-left font-semibold">Quantity</th>
+                              <th class="text-left font-semibold">Total</th>
+                          </tr>
+                      </thead>
+                      <tbody>
+
+                        {
+                          cartItem?.map((item,index)=>
+                          <tr>
+                          <td class="py-4">
+                              <div class="flex ">
+                                  <img class="h-16 w-16 mr-4 " src={item?.image} alt="Product image"/>
+                                  <span class="font-semibold">{item?.category.substring(0, 16)}</span>
+                              </div>
+                          </td>
+                          <td class="py-4">{item?.price}</td>
+                          <td class="py-4">
+                              <div class="flex items-center">
+                                  <button class="border rounded-md py-2 px-4 mr-2">-</button>
+                                  <span class="text-center w-8">{item?.qnty}</span>
+                                  <button class="border rounded-md py-2 px-4 ml-2">+</button>
+                              </div>
+                          </td>
+                          <td class="py-4">{item?.price * item?.qnty}</td>
+                      </tr>)
+                        }
+                          
+                      </tbody>
+                  </table>
+              </div>
           </div>
-    }
-   
+            }
+           {cartItem?.length > 0 && <div class="md:w-1/4">
+                <div class="bg-white rounded-lg shadow-md p-6">
+                    <h2 class="text-lg font-semibold mb-4">Summary</h2>
+                    <div class="flex justify-between mb-2">
+                        <span>Subtotal</span>
+                        <span>{total.toFixed(2)}</span>
+                    </div>
+                    <div class="flex justify-between mb-2">
+                        <span>Taxes</span>
+                        <span>$1.99</span>
+                    </div>
+                    <div class="flex justify-between mb-2">
+                        <span>Shipping</span>
+                        <span>$0.00</span>
+                    </div>
+                    <hr class="my-2"/>
+                    <div class="flex justify-between mb-2">
+                        <span class="font-semibold">Total</span>
+                        <span class="font-semibold">{total.toFixed(2)}</span>
+                    </div>
+                    <button class="bg-blue-500 text-white py-2 px-4 rounded-lg mt-4 w-full">Checkout</button>
+                </div>
+            </div>}
+        </div>
     </div>
-    </div>
+</div>
+    
+    </>
   );
 };
 
--- src/page/Slider.jsx
+++ src/page/Slider.jsx
@@ -1,87 +1,205 @@
 import React from 'react'
 import { Carousel, Card, Button } from 'react-bootstrap'
+// import Carousel from 'react-bootstrap/Carousel'
 import image1 from '../images/shoping1.jpg'
 import image2 from '../images/shoping.jpg'
 import image3 from '../images/shoping2.jpg'
+import vagitables from '../images/vagitables3.png'
 
 import image4 from '../images/shoping3.jpg'
 import image5 from '../images/shoping5.jpg'
 import './Common.css'
 
 
+const slideItem = [
+  {
+    title: 'Fresh Vagetables',
+    description: 'Nulla vitae elit libero, a pharetra augue mollis interdum.',
+    image: image1
+  },
+  {
+    title: 'Latest & Updates Products',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    image: image2
+  },
+  {
+    title: 'All Updates products ',
+    description: 'Praesent commodo cursus magna, vel scelerisque nisl consectetur.',
+    image: image3
+  }
+]
+
 export const Slider = () => {
+
+
+   // Set the date we're counting down to
+   var countDownDate = new Date("Jan 1, 2024 00:00:00").getTime();
+
+   // Update the countdown every 1 second
+   var x = setInterval(function() {
+       // Get today's date and time
+       var now = new Date().getTime();
+       // Find the distance between now and the count down date
+       var distance = countDownDate - now;
+       // Time calculations for days, hours, minutes and seconds
+       var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+       var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+       var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+       var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+       // Display the result in the corresponding elements
+       document.getElementById("days").textContent = days + "d";
+       document.getElementById("hours").textContent = hours + "h";
+       document.getElementById("minutes").textContent = minutes + "m";
+       document.getElementById("seconds").textContent = seconds + "s";
+
+      //  // Display the result in the corresponding elements
+      //  document.getElementById("days1").textContent = days + "d";
+      //  document.getElementById("hours1").textContent = hours + "h";
+      //  document.getElementById("minutes1").textContent = minutes + "m";
+      //  document.getElementById("seconds1").textContent = seconds + "s";
+
+
+       // If the count down is over, write some text 
+       if (distance < 0) {
+           clearInterval(x);
+           document.getElementById("countdown").innerHTML = "EXPIRED";
+       }
+   }, 1000);
+
+
+
+   var countDownDate1 = new Date("oct 1, 2023 00:00:00").getTime();
+   var y= setInterval(function() {
+    // Get today's date and time
+    var now = new Date().getTime();
+    // Find the distance between now and the count down date
+    var distance = countDownDate1 - now;
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  
+
+    // Display the result in the corresponding elements
+    document.getElementById("days1").textContent = days + "d";
+    document.getElementById("hours1").textContent = hours + "h";
+    document.getElementById("minutes1").textContent = minutes + "m";
+    document.getElementById("seconds1").textContent = seconds + "s";
+
+
+    // If the count down is over, write some text 
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("countdown").innerHTML = "EXPIRED";
+    }
+}, 1000);
+
+   
   return (
-    <div className='row' style={{ background: 'white', marginTop: '5px', marginLeft: '2px', marginRight: '2px' }}>
+    <div className='row mx-[2px]' style={{ background: 'white', marginTop: '5px',  }}>
       <div className='col-md-8 slide' style={{ height: '380px' }}>
-        <Carousel >
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src={image3}
-              alt="First slide"
-              style={{ height: '360px', borderRadius: '15px' }}
-            />
-            <Carousel.Caption >
-              <h3 style={{ color: '#eb4334' }}>Fresh Vagetables</h3>
-              <p style={{ marginLeft: '30px', fontWeight: 'bold' }}>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-              <Button style={{ backgroundColor: 'green', borderRadius: '5px', color: 'white' }} className='btn-1'>Shop Now</Button>
+        <Carousel nextIcon={null} prevIcon={null} variant='dark' interval={3000}>
+          {
+            slideItem?.map((item, index) => (
+              <Carousel.Item>
 
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              style={{ height: '360px', borderRadius: '15px', }}
-              className="d-block w-100"
-              src={image1}
-              alt="Second slide"
-            />
 
-            <Carousel.Caption>
-              <h3 style={{ color: '#eb4334' }}>Latest & Updates Products</h3>
-              <p >Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-              <Button className='btn-1' style={{ backgroundColor: '#09801b', borderRadius: '5px', color: 'white' }}>Shop Now</Button>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              style={{ height: '360px', borderRadius: '15px' }}
-              className="d-block w-100"
-              src={image2}
-              alt="Third slide"
-            />
+                <section class="text-gray-600 body-font bg-gray-200 dark:bg-slate-900 rounded-md shadow-sm h-[360px]">
+                  <div class="container mx-auto flex md:px-24 md:py-10 md:flex-row flex-col items-center">
+                    <div
+                      class=" hidden md:block lg:flex-grow mt-5 md:mt-0   md:w-1.5/2 lg:pr-24 md:pr-16  flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+                      <h1
+                        class="text-2xl font-extrabold leading-9 tracking-tight mb-3 text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-5xl md:leading-normal">
+                        Title of Project
+                      </h1>
+                      <p class="mb-8 md:pl-0  pl-2 pr-2 leading-relaxed dark:text-gray-300">
+                        Short description here, Short description here Short description here Short description here Short
+                        description here Short description here.
+                      </p>
+                      <div class="flex justify-center">
+                        <a href="#"
+                          class="inline-flex text-white bg-emerald-600 border-0 py-2 px-6 focus:outline-none hover:bg-emerald-600 rounded text-lg">Start
+                          Exploring
+                        </a>
+                        <a href="#"
+                          class="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">Read
+                          articles</a>
+                      </div>
+                    </div>
+                    <div class=" lg:max-w-lg lg:w-full mb-5 md:mb-0 md:w-1/2 w-5/6">
+                      <img class="object-cover object-center rounded d-block w-100" alt="hero" src="https://www.svgrepo.com/show/490900/hot-air-balloon.svg" />
+                    </div>
+                  </div>
+                </section>
 
-            <Carousel.Caption>
-              <h3 style={{ color: '#eb4334' }}>All Updates products </h3>
-              <p >Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-              <Button className='btn-1' style={{ backgroundColor: '#09801b', borderRadius: '5px', color: 'white' }}>Shop Now</Button>
-            </Carousel.Caption>
-          </Carousel.Item>
+              </Carousel.Item>
+            ))
+          }
         </Carousel>
       </div>
 
-      <div className='col-md-4 ' style={{ border: '2px solid tomato', height: '360px' }} id="showdiv">
-        <div style={{ background: '#f0adad', height: '30px' }}>
-          <h6 style={{ marginRight: '50px', }}>Latest Super Discount Active Coupon Code</h6>
-
+      <div className='col-md-4 rounded-md bg-[#F9FAFB] ' style={{ border: '2px solid tomato', height: '360px' }} id="showdiv">
+      <h6 className='bg-orange-300 w-[100%] p-2  font-semibold rounded-sm' >Latest Super Discount Active Coupon Code</h6>
+        <div  className=' flex width-auto h-[140px] mt-[15px] shadow-md p-[20px] gap-3 rounded-sm' >
+          <img src={image5} alt='card-image' className='h-[90px] w-[50%] rounded-md'/>
+          <div>
+            <div className='flex gap-2'>
+              <p className='text-2xl text-red-600 font-semibold'>$50 </p><span className='text-[20px]'>Off </span> 
+              <button className='bg-[#D1FAE5] rounded-full w-[70px] p-0'>active </button>
+            </div>
+            <h2 className='text-start font-semibold'>SUMMER23</h2>
+            <div class="flex flex-wrap gap-2 justify-between items-center pt-2 ">
+                <div class="border rounded-md px-1 bg-orange-300">
+                    <div id="days" class="font-bold font-mono text-sm text-gray-800"></div>
+                </div>
+                <div class="border rounded-md px-1 bg-orange-300">
+                    <div id="hours" class="font-bold font-mono text-sm text-gray-800"></div>
+                </div>
+                <div class="border rounded-md px-2 bg-orange-300">
+                    <div id="minutes" class="font-bold font-mono text-sm text-gray-800"></div>
+                </div>
+                <div class="border rounded-md px-1 bg-orange-300">
+                    <div id="seconds" class="font-bold font-mono text-sm text-gray-800"></div>
+                </div>
+            </div>
+          </div>
         </div>
 
-        <Card style={{ width: 'auto', height: '140px', marginTop: '10px' }} >
-          <Card.Img variant="top" src={image5} style={{ height: '135px' }} />
-          <Card.Body>
 
-          </Card.Body>
-        </Card>
+        <div  className=' flex width-auto h-[140px] mt-[10px] shadow-md p-[20px] gap-3 rounded-sm'>
+          <img src={vagitables} alt='card-image' className='h-[90px] w-[50%] rounded-md'/>
+          <div>
+            <div className='flex gap-2'>
+              <p className='text-2xl text-red-600 font-semibold'>15% </p><span className='text-[20px]'>Off </span> 
+              <button className='bg-[#D1FAE5] rounded-full w-[70px] p-0'>active </button>
+            </div>
+            <h2 className='text-start font-semibold'>Summer Gift Coupn</h2>
+            <div class="flex flex-wrap gap-2 justify-between items-center pt-2 ">
+                <div class="border rounded-md px-1 bg-orange-300">
+                    <div id="days1" class="font-bold font-mono text-sm text-gray-800"></div>
+                </div>
+                <div class="border rounded-md px-1 bg-orange-300">
+                    <div id="hours1" class="font-bold font-mono text-sm text-gray-800"></div>
+                </div>
+                <div class="border rounded-md px-2 bg-orange-300">
+                    <div id="minutes1" class="font-bold font-mono text-sm text-gray-800"></div>
+                </div>
+                <div class="border rounded-md px-1 bg-orange-300">
+                    <div id="seconds1" class="font-bold font-mono text-sm text-gray-800"></div>
+                </div>
+            </div>
+          </div>
+        </div>
 
-        <Card style={{ width: 'auto', height: '140px', marginTop: '10px' }}>
-          <Card.Img variant="top" src={image4} style={{ height: '135px' }} />
-          <Card.Body>
-          </Card.Body>
-        </Card>
+    
 
       </div>
 
 
-      <div className="row " style={{ backgroundColor: '#f5afa9', borderRadius: '5px', marginBottom: '20px', marginLeft: '2px', }}>
+      <div className="row bg-[#f5afa9] rounded-md mb-[20px] ml-[5px] mr-[5px] p-[10px]" >
         <div className='col-md-8'>
           <span>  <h4 ><strong ><span style={{ color: 'green' }}>100% Natural Quality</span></strong> Organic Product</h4> </span>
           <p >See Our latest discounted products from here and get a specialdiscount product </p>
