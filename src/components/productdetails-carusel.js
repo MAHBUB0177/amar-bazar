5c68522b53ea9e33f7b9dcb6b54190760348d585
mahbub alam <ma01776879@gmail.com>
2023-09-03 17:13:02 +0600

    update

--- src/components/productdetails-carusel.js
+++ src/components/productdetails-carusel.js
@@ -5,27 +5,31 @@ export const ProductdetailsCarusel = ({productDetails}) => {
 
     const data = [
         {
-            image:productDetails?.image,
-                // "https://cdn.tgdd.vn/Products/Images/42/209800/oppo-reno2-f-xanh-1-org.jpg",
+            image:
+            // productDetails?.image,
+                "https://cdn.tgdd.vn/Products/Images/42/209800/oppo-reno2-f-xanh-1-org.jpg",
             text: "img1"
         },
         {
-            image:productDetails?.image,
-                // "https://cdn.tgdd.vn/Products/Images/42/209800/oppo-reno2-f-xanh-4-org.jpg",
+            image:
+            // productDetails?.image,
+                "https://cdn.tgdd.vn/Products/Images/42/209800/oppo-reno2-f-xanh-4-org.jpg",
             text: "img2"
         },
         {
-            image:productDetails?.image,
-                // "https://cdn.tgdd.vn/Products/Images/42/209800/oppo-reno2-f-xanh-10-org.jpg",
+            image:
+            // productDetails?.image,
+                "https://cdn.tgdd.vn/Products/Images/42/209800/oppo-reno2-f-xanh-10-org.jpg",
             text: "img3"
         }
     ];
   return (
     <div>
-<SliderImage
+           <SliderImage
             data={data}
-            width="500px"
+            width="500"
             showDescription={true}
+            // height="200" 
             direction="right"
         />
 
